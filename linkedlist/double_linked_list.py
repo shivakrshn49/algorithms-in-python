c98a7d1ae55e9d915462967ecfa48e8edcebe62f
shivakrshn49@gmail.com <shivakrshn49@gmail.com>
2019-12-24 21:55:13 +0530

    Add double linked list implementation

--- linkedlist/double_linked_list.py
+++ linkedlist/double_linked_list.py
@@ -1,26 +1,28 @@
 # Sources for ideas 
-# 1. https://stackabuse.com/linked-lists-in-detail-with-python-examples-single-linked-lists/
+# 1. https://stackabuse.com/doubly-linked-list-with-python-examples/
 # 2. https://www.geeksforgeeks.org/data-structures/linked-list/
 
 class Node(object):
     """
-    Node object stores two elememts
-    1. data
-    2. Reference to next node object
+    Node object stores three elememts
+    1. Reference to prev node object
+    2. data
+    3. Reference to next node object
     """
     
     def __init__(self, data):
+        # Initial reference to prev node object is None
+        self.prev_node_ref = None
         self.data = data
         # Initial reference to next node object is None, because we will handle assinging
         # new node to existing node using another class (Linked list)
         self.next_node_ref = None
 
 
-class SingleLinkedList(object):
+class DoubleLinkedList(object):
     """
-    Single linked list is data structure that stores the data in the form of 
-    node and maintains a reference to next node(or data) but not the previous 
-    node(or data)
+    Double linked list is data structure that stores the data in the form of 
+    node and maintains a reference to both prev and next node(or data) 
     """
 
     def __init__(self):
@@ -31,13 +33,13 @@ class SingleLinkedList(object):
         # we should increase it for each and every insert, so that
         # we can track the length without traversing the linked list
         self._length = 0
-        # Keep track of last element
+        # Keep track of last element/node
         self.last_node = None 
 
 
     def check_if_head_node_exists(self):
         if self.root is None:
-            print("No elememts present in the single linked list")
+            print("No elememts present in the double linked list")
             return
 
     def insert(self, data, location="end"):
@@ -57,15 +59,20 @@ class SingleLinkedList(object):
         if location == "start":
             # Make the root node as next node of new node
             new_node.next_node_ref = self.root
+            self.root.prev_node_ref = new_root_node
+            # Increase the length of linked list
             self._length += 1
             # Make the root node as new node
             self.root = new_node
         else:
             # Assing the new node to the last node
             self.last_node.next_node_ref = new_node
-            self._length += 1
+            # Assing the last as prev ref to new node 
+            new_node.prev_node_ref = self.last_node
             # Make the last node as new node
             self.last_node = new_node
+            # Increase the length
+            self._length += 1
 
     def delete(self, location="end"):
         """
@@ -78,44 +85,49 @@ class SingleLinkedList(object):
             new_root_node = self.root.next_node_ref
             # Remove the reference from the root node
             self.root.next_node_ref = None
+            new_root_node.prev_node_ref = None
             # Assing the new root node
             self.root = new_root_node
             self._length -= 1
         else:
+            prev_node = self.last_node.prev_node_ref
+            self.last_node.prev_node_ref = None
+            prev_node.next_node_ref = None
+            self.last_node = prev_node
             current_node = self.root
-            while current_node:
-                if current_node.next_node_ref.next_node_ref is None:
-                    current_node.next_node_ref = None
-                    self._length -= 1
-                current_node = current_node.next_node_ref
+            self._length -= 1
 
     def delete_node_with_data(self, data):
         self.check_if_head_node_exists()
         current_node = self.root
-        prev_node = None
         while current_node:
+            next_node = current_node.next_node_ref
+            prev_node = current_node.prev_node_ref
             if current_node.data == data:
-                prev_node.next_node_ref = current_node.next_node_ref
-                # Remove the reference
-                current_node.next_node_ref = None
+                current_node.prev_node_ref = current_node.next_node_ref = None
+                prev_node.next_node_ref = next_node
+                next_node.prev_node_ref = prev_node
                 self._length -= 1
                 break
-            prev_node = current_node
-            current_node = current_node.next_node_ref
+            current_node = next_node
 
     def travese_list(self):
         """
         Go through each and every element of the list and print it
         """
+        # Check if root / head node exists, if not print "no elements"
+        # and return nothing
+        self.check_if_head_node_exists()
+
         # Take inital node as root node
-        node = self.root
+        root_node = self.root
         self.final_list = []
-        while node:
+        while root_node:
             # As long as the node exists, print the value of the 
             # node and track/refer the next node
-            print(node.data)
-            self.final_list.append(node.data)
-            node = node.next_node_ref
+            print(root_node.data)
+            self.final_list.append(root_node.data)
+            root_node = root_node.next_node_ref
 
         return self.final_list
 
@@ -162,7 +174,7 @@ class SingleLinkedList(object):
 
 
 
-linked_list = SingleLinkedList()
+linked_list = DoubleLinkedList()
 
 for i in range(1,6):
     # Insert at end of the list
